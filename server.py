41135dfaf17bf60efc6ed392f8be3692114f6b36
ChocoDrusha <andrey.evlashev@mail.ru>
2022-02-25 07:56:57 +0300

    Rewrite server.py

--- server.py
+++ server.py
@@ -1,14 +1,45 @@
 import socket
+from _thread import *
+import threading
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('0.0.0.0', 5055))
-clients = []
-while True:
-    data, addres = sock.recvfrom(1024)
-    print(addres[0], addres[1])
-    if addres not in clients:
-        clients.append(addres)
-    for client in clients:
-        if client == addres:
-            continue
-        sock.sendto(data, client)
+print_lock = threading.Lock()
+
+def threaded(c):
+    while True:
+
+        data = c.recv(1024)
+        if not data:
+            print('Poka')
+
+            print_lock.release()
+            break
+
+        data = data[::-1]
+
+        c.send(data)
+
+    c.close()
+
+def main():
+    host = socket.gethostname()
+
+    port = 3333
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    print(f'listrning on {host}:{port}')
+
+    s.listen(5)
+    print('socket listening')
+
+    while True:
+
+        c, addr = s.accept()
+
+        print_lock.acquire()
+        print('Connected to:', addr[0], addr[1])
+        start_new_thread(threaded, (c,))
+
+    s.close()
+
+if __name__ == '__main__':
+    main()
